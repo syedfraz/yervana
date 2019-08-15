9eb16b542ba9949631cc14834eab80664a871d32
Fraz <sftahir@sfu.ca>
2019-08-14 18:05:18 -0700

    yervana component

---
+++ yervana/src/Component/yervana.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Yervana extends Component {
+  render() {
+    return (
+      <div className="Yervana">
+
+          <h3>YERVANA</h3>
+      </div>
+    );
+  }
+}
+
+export default Yervana;
